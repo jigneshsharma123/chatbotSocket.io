02c4d59aeb2ce60c4f036c6af78df79097fd3a26
jignesh sharma <jigneshsharma9868@gmail.com>
2023-11-15 14:35:51 +0530

    modifiying the uri of the mogooseConnection

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-mongoose.connect(process.env.MONGO_DB_URI);
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'error in connecting to the db'));
-db.once('open',()=> {
-    console.log("Successfully Connected to the DB");
+const connectionOptions = {
+    useNewUrlParser : true,
+    useUnifiedTopology:true,
+    ssl : true,
+}
+mongoose.connect(process.env.MONGO_DB_URI).then(()=> {
+    console.log('Connected to the db');
+}).catch((error)=> {
+    console.error('error in connecting to mongodb', error);
 });
-module.exports = db;
+
+
+
+
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'error in connecting to the db'));
+// db.once('open',()=> {
+//     console.log("Successfully Connected to the DB");
+// });
+// module.exports = db;
